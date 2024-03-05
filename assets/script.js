e1696faff4b9a8f3ccf4cdeba80cbe04361c6ec4
Javier Guarda <guardaajavier@gmail.com>
2024-03-05 20:01:54 -0300

    pimer repo de ricomida

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function cambiar_titulo1 () {
+    $('#titulo1').addClass('text-danger')
+}
+
+$('#titulo1').on('dblclick', cambiar_titulo1)
+
+function cambiar_titulo2 () {
+    $('#titulo2').addClass('text-danger')
+}
+
+$('#titulo2').on('dblclick', cambiar_titulo2)
+
+function alerta() {
+    alert('Email enviado con exito...')
+}
+$('#enviarAlert').on('click', alerta)
+
+function desaparecer_aparecer_tarjeta() {
+    $(".card-text").toggle(); 
+}
+$("#tarjeta, #tarjeta2, #tarjeta3").on('click', desaparecer_aparecer_tarjeta)
+
+
+// $("#tarjeta, #tarjeta2, #tarjeta3").on('click', function(){
+//     $(".card-text").toggle(); 
+// });
+
+
